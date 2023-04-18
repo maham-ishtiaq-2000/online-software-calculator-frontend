105217be9bd76837466642691f73a30fbb4cd27b
maham-ishtiaq-2000 <mahamishtiaq2000@gmail.com>
2023-04-18 06:29:26 -0700

    myfirstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+
+const App = () => {
+  let [streetName , setStreetName] = useState("")
+  let [streetNumber , setStreetNumber] = useState("")
+  let [assessedValue , setAssessedValue] = useState("")
+  let [conditionalRating , setConditionalRating] = useState("")
+  const handleStreetNumber = (e) => {
+    setStreetNumber(e.target.value)
+  }
+  const handleStreetName = (e) => {
+    setStreetName(e.target.value)
+  }
+  const handleConditionalRating = (e) =>{
+    setConditionalRating(e.target.value)
+  }
+  const submit = (event) => {
+    event.preventDefault()
+    console.log(streetName)
+    console.log(streetNumber)
+    console.log(conditionalRating)
+    console.log("submitHandler")
+    
+    const link = "https://confused-dog-poncho.cyclic.app/home"
+    axios.post(link,
+        {
+          streetNumber: streetNumber,
+          streetName : streetName
+
+        })
+        .then((res) => {
+            var receivedValue = res.data.data
+            receivedValue = receivedValue.substring(1)
+            receivedValue = Number(receivedValue.replace(/,/g, ''))
+            if(conditionalRating == "1"){
+              console.log(0.6 * assessedValue)
+              console.log("from here")
+              setAssessedValue(receivedValue * 0.6)
+            }
+            else if(conditionalRating == "2"){
+              console.log("from here")
+              console.log(0.8 * assessedValue)
+              setAssessedValue(receivedValue * 0.8)
+            }
+            else{
+              console.log(0.9 * assessedValue)
+              setAssessedValue(receivedValue * 0.9)
+            }
+        })
+  }
+  
+  console.log(assessedValue)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div>CALCULATOR</div>
+        <form>
+            <label>Enter street number : </label>
+            <input type="text" onChange = {handleStreetNumber}></input>
+            <br></br>
+            <label>Enter street name : </label>
+            <input type="text" onChange = {handleStreetName}></input>
+            <br></br>
+            <label for="cars">Choose Conditional Rating:</label>
+
+              <select onChange={handleConditionalRating}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+        </form>
+        <button onClick={submit}>submit</button>
+        <p>assessedValue : {assessedValue}</p>
+    </>
+    
   );
-}
+};
 
 export default App;
